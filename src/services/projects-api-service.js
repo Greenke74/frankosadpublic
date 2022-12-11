6a7fa46a23d0e1d281c852203cb08f0600fc8277
rost <rsharamailuk13@gmail.com>
2022-12-11 09:37:17 +0200

    fix for get project with blocks

--- src/services/projects-api-service.js
+++ src/services/projects-api-service.js
@@ -37,16 +37,33 @@ export const getProjects = () => new Promise((resolve, reject) => {
 	}
 })
 
-export const getProjectWithBlocks = (value) => new Promise((resolve, reject) => {
+export const getProjectWithBlocksById = (id) => new Promise((resolve, reject) => {
 	try {
-		supabase.rpc('get_project_with_blocks', value)
-			.then(({ data, error }) => {
-				if (error) {
-					reject(error);
-				}
-				resolve(data);
-			})
-			.catch(error => reject(error))
+
+		supabase.rpc('get_project_with_blocks_by_id', {_id: id})
+		.then(response => {
+			if (response.error) {
+				reject(response.error.message)
+			}
+			resolve(response)
+		})
+		.catch(error => reject(error))
+	} catch (e) {
+		reject(e)
+	}
+})
+
+export const getProjectWithBlocksByAlias = (alias) => new Promise((resolve, reject) => {
+	try {
+
+		supabase.rpc('get_project_with_blocks_by_alias', {_alias: alias})
+		.then(response => {
+			if (response.error) {
+				reject(response.error.message)
+			}
+			resolve(response)
+		})
+		.catch(error => reject(error))
 	} catch (e) {
 		reject(e)
 	}
