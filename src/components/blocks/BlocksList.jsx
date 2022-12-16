862f714739b33141b746425084349da0fa0ea992
Andriy Pavlykivskyi <greenke74@gmail.com>
2022-12-16 18:11:27 +0200

    delete unused imports

--- src/components/blocks/BlocksList.jsx
+++ src/components/blocks/BlocksList.jsx
@@ -2,46 +2,41 @@ import React, { useState, useEffect, Suspense } from 'react';
 import PageBlock from './PageBlock.jsx';
 import { getBlocks } from './blocks.js';
 import { Spinner } from '../common/Spinner.jsx';
-import { Links, navLinks } from '../../services/nav-routes-service.js';
-import { Breadcrumbs, Typography } from '@mui/material';
-import { Link, useLocation } from 'react-router-dom';
-import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
-import useReactRouterBreadcrumbs from 'use-react-router-breadcrumbs';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import { styled } from '@mui/system';
 
 const HomeIcon = styled(HomeOutlinedIcon)({
-  height: '35px',
-  width: '35px',
-  display: 'flex',
-  alignItems: 'center'
+	height: '35px',
+	width: '35px',
+	display: 'flex',
+	alignItems: 'center'
 })
 
 const BlocksList = ({ blocksData, blockTypes }) => {
-  const [blocks, setBlocks] = useState([]);
+	const [blocks, setBlocks] = useState([]);
 
-  useEffect(() => {
-    setBlocks(blockTypes.map(blockType => getBlocks(blockType)).filter(block => block))
-  }, [blockTypes])
+	useEffect(() => {
+		setBlocks(blockTypes.map(blockType => getBlocks(blockType)).filter(block => block))
+	}, [blockTypes])
 
-  return (
-    <>
-      {blocks.map((block, idx) => {
-        const Component = block?.component;
-        const blockData = blocksData?.find(blockData => blockData?.type === block?.type)
+	return (
+		<>
+			{blocks.map((block, idx) => {
+				const Component = block?.component;
+				const blockData = blocksData?.find(blockData => blockData?.type === block?.type)
 
-        return Component && (
-          <Suspense fallback={<Spinner />} key={`${idx}`} >
-            <PageBlock
-              component={Component}
-              fullWidth={block.fullWidth}
-              blockData={blockData}
-            />
-          </Suspense>
-        )
-      })}
-    </>
-  )
+				return Component && (
+					<Suspense fallback={<Spinner />} key={`${idx}`} >
+						<PageBlock
+							component={Component}
+							fullWidth={block.fullWidth}
+							blockData={blockData}
+						/>
+					</Suspense>
+				)
+			})}
+		</>
+	)
 }
 
 export default BlocksList
