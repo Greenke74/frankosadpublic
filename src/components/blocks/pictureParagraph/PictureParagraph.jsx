a03751e11149f8bbbc1debf93d7b2b75d30c4376
Andriy Pavlykivskyi <greenke74@gmail.com>
2022-12-27 09:40:50 +0200

    fix picture paragraph block

--- src/components/blocks/pictureParagraph/PictureParagraph.jsx
+++ src/components/blocks/pictureParagraph/PictureParagraph.jsx
@@ -1,28 +1,38 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
+import { borderRadius } from '@mui/system';
 import React from 'react'
 import { getImageSrc } from '../../../services/settingApiService'
 
+const IMAGE_ASPECT_RATIO = 5 / 3;
+
 const PictureParagraph = ({ blockData }) => {
 
+  console.log(blockData?.data?.paragraph);
   return (
-    <Box
-      display={'flex'}
-      gap={'45px'}
-      bgcolor={'var(--block-background-color)'}
-      padding={'45px'}
-      borderRadius={'var(--page-border-radius)'}
-    >
-      <img
-        src={getImageSrc(blockData?.data?.image)}
-        alt='image'
-        style={{
-          width: 'calc(var(--max-content-width) / 2)',
-          minWidth:'200px',
-          aspectRatio: '3/1',
-          borderRadius:'var(--block-border-radius)'
-        }}
-      />
-      <Typography fontSize={24}>{blockData?.data?.paragraph} </Typography>
+    <Box sx={{
+      bgcolor: 'var(--block-background-color)',
+      borderRadius: 'var(--page-border-radius)',
+      padding: '45px'
+    }}>
+      <Grid
+        container
+        spacing='45px'
+      >
+        <Grid item xs={12} md={6} sx={{order: blockData?.data?.imageFirst? undefined : 1}}>
+          <img
+            src={getImageSrc(blockData?.data?.image?.url)}
+            alt='image'
+            style={{
+              aspectRatio: `${IMAGE_ASPECT_RATIO}`,
+              borderRadius: 'var(--block-border-radius)',
+              width:'100%'
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} >
+          <Typography fontSize={24} whiteSpace='break-spaces'>{blockData?.data?.paragraph}</Typography>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
